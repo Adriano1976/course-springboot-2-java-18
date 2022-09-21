94b252272df2274276006e5f39f9c890b340daeb
Adriano Santos <adriradiologista@hotmail.com.br>
2022-09-21 09:19:53 -0300

    Create Product, Order e Category delete methods

--- src/main/java/com/workshop/course/services/CategoryService.java
+++ src/main/java/com/workshop/course/services/CategoryService.java
@@ -2,7 +2,11 @@ package com.workshop.course.services;
 
 import com.workshop.course.entities.Category;
 import com.workshop.course.repositories.CategoryRepository;
+import com.workshop.course.services.exceptions.DatabaseException;
+import com.workshop.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,5 +29,17 @@ public class CategoryService {
 
     public Category insert(Category objeto) {
         return repository.save(objeto);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ResourceNotFoundException(id);
+
+        } catch (DataIntegrityViolationException exception) {
+            throw new DatabaseException(exception.getMessage());
+        }
     }
 }
--- src/main/java/com/workshop/course/services/OrderService.java
+++ src/main/java/com/workshop/course/services/OrderService.java
@@ -2,7 +2,11 @@ package com.workshop.course.services;
 
 import com.workshop.course.entities.Order;
 import com.workshop.course.repositories.OrderRepository;
+import com.workshop.course.services.exceptions.DatabaseException;
+import com.workshop.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,5 +29,17 @@ public class OrderService {
 
     public Order insert(Order objeto) {
         return repository.save(objeto);
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ResourceNotFoundException(id);
+
+        } catch (DataIntegrityViolationException exception) {
+            throw new DatabaseException(exception.getMessage());
+        }
     }
 }
